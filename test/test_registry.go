e01fbaf80bd74f7e973cb89860057ff459443790
chadhao <haoduan@outlook.com>
2017-12-01 18:20:09 +1300

    [update]add registry test

--- test/test_registry.go
+++ test/test_registry.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/nzgogo/micro/registry"
+)
+
+func main() {
+	reg := registry.NewRegistry()
+
+	var srv registry.Service
+	srv.ID = "01"
+	srv.Name = "Nats"
+	srv.Address = "127.0.0.1"
+	srv.Port = 4222
+
+	err := reg.Register(&srv)
+
+	if err != nil {
+		fmt.Printf("[Error]%s\n", err)
+	}
 }
