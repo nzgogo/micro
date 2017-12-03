9152501c33016cb0d1fba6245542519a88140fde
Feng Zhu <feng.zhu1980@gmail.com>
2017-12-03 17:04:19 +1300

    Add deregistry function in registry.go file. Add test_deregistry.go file

--- test/test_deregistry.go
+++ test/test_deregistry.go
@@ -1,10 +1,7 @@
-// +build ignore
-
 package main
 
 import (
 	"fmt"
-
 	"micro/registry"
 )
 
@@ -13,13 +10,9 @@ func main() {
 
 	var srv registry.Service
 	srv.ID = "01"
-	srv.Name = "Nats"
-	srv.Address = "127.0.0.1"
-	srv.Port = 4222
-
-	err := reg.Register(&srv)
+	err := reg.Deregister(&srv)
 
 	if err != nil {
-		fmt.Printf("[Error]%s\n", err)
+		fmt.Printf("[Error]%s \n", err)
 	}
 }
