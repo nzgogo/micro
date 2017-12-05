336362fa3d02e0aa741059d09f9101ebc4f7d536
QieKai <qiekai@yahoo.com>
2017-12-05 19:25:02 +1300

    Add service discovery

--- test/test_registry.go
+++ test/test_registry.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"fmt"
-
+	"github.com/hashicorp/consul/api"
 	"micro/registry"
 )
 
@@ -16,10 +16,31 @@ func main() {
 	srv.Name = "Nats"
 	srv.Address = "127.0.0.1"
 	srv.Port = 4222
-
+	srv.Check = &api.AgentServiceCheck{
+		Script:     "ls",
+		Interval: "5s",
+		Timeout:  "1s",
+	}
 	err := reg.Register(&srv)
 
 	if err != nil {
 		fmt.Printf("[Error]%s\n", err)
+	}
+
+
+	var srv1 registry.Service
+	srv1.ID = "02"
+	srv1.Name = "Nats"
+	srv1.Address = "127.0.0.1"
+	srv1.Port = 4223
+	srv1.Check = &api.AgentServiceCheck{
+		Script:     "fuck up",
+		Interval: "5s",
+		Timeout:  "1s",
+	}
+	err1 := reg.Register(&srv1)
+
+	if err1 != nil {
+		fmt.Printf("[Error]%s\n", err1)
 	}
 }
