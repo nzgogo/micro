d3291e203a4431f8ff68f4dbc48ffebdf27e4e79
chadhao <haoduan@outlook.com>
2017-12-19 13:11:43 +1300

    [update]

--- service.go
+++ service.go
@@ -4,7 +4,7 @@ type Service interface {
 	Options() Options
 	Init(...Options) error
 	Run() error
-	Close() error
+	Close()
 }
 
 type service struct {
@@ -12,4 +12,23 @@ type service struct {
 	Name    string
 	Version string
 	ID      string
+}
+
+func (s *service) Options() Options {
+	return s.opts
+}
+
+func (s *service) Init(opts ...Options) error {
+	return nil
+}
+
+func (s *service) Run() error {
+	return nil
+}
+
+func (s *service) Close() {
+}
+
+func NewService() *service {
+	return &service{}
 }
