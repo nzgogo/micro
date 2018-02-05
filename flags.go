99f22d1e2e3aa289789d709642f7c5eb6ea46c84
chadhao <chad.hao@gogoexpress.co.nz>
2018-02-05 16:16:01 +1300

    [debug]

--- flags.go
+++ flags.go
@@ -1,14 +1,23 @@
 package gogo
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
 
 func parseFlags(s *service) {
 	if s.config["nats_addr"] == "" {
 		s.config["nats_addr"] = "nats://127.0.0.1:4222"
 	}
 
-	s.config["consul_addr"] = *flag.String("consul", s.config["consul_addr"], "Consul server address")
-	s.config["nats_addr"] = *flag.String("nats", s.config["nats_addr"], "Nats server address")
+	// s.config["consul_addr"] = *flag.String("consul", s.config["consul_addr"], "Consul server address")
+	// s.config["nats_addr"] = *flag.String("nats", s.config["nats_addr"], "Nats server address")
+
+	a := *flag.String("consul", s.config["consul_addr"], "Consul server address")
+	b := *flag.String("nats", s.config["nats_addr"], "Nats server address")
+
+	fmt.Println(a)
+	fmt.Println(b)
 
 	flag.Parse()
 }
