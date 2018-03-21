ae435ea978f0c01c5d2299a73cd6fc076409d094
QieKai <qiekai@yahoo.com>
2018-03-21 15:30:53 +1300

    add mongoDB driver support

---
+++ db/mongodb.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"crypto/tls"
+	"github.com/globalsign/mgo"
+	"net"
+	"strings"
+)
+
+type MgoDB interface {
+	Connect() error
+	Close()
+	DB() *mgo.Session
+}
+
+type mgodb struct {
+	conn     *mgo.Session
+	opts     Options
+	dialInfo *mgo.DialInfo
+}
+
+func (d *mgodb) Connect() error {
+	var tlsConfig *tls.Config
+	if d.opts.TLS != nil {
+		tlsConfig = d.opts.TLS
+	} else {
+		tlsConfig = &tls.Config{}
+		tlsConfig.InsecureSkipVerify = true
+	}
+
+	if d.opts.sslMgo {
+		d.dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+			conn, err := tls.Dial(d.opts.Protocol, addr.String(), tlsConfig)
+			return conn, err
+		}
+	}
+	var err error
+	d.conn, err = mgo.DialWithInfo(d.dialInfo)
+	return err
+}
+
+func (d *mgodb) Close() {
+	d.conn.Close()
+}
+
+func (d *mgodb) DB() *mgo.Session {
+	return d.conn
+}
+
+func NewMongoDB(url string, opts ...Option) *mgodb {
+	options := Options{
+		Protocol: DefaultProtocol,
+		sslMgo:   strings.Contains(url, "ssl=true"),
+	}
+	url = strings.Replace(url, "ssl=true", "", -1)
+	dialOp, err := mgo.ParseURL(url)
+	if err != nil {
+		panic("Failed to parse URI: " + err.Error())
+	}
+
+	for _, o := range opts {
+		o(&options)
+	}
+
+	return &mgodb{
+		opts:     options,
+		dialInfo: dialOp,
+	}
+}
